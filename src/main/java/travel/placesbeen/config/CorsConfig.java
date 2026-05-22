781f1f17763de05f72301499e80dbbd45a2c6ae7
AaronCur <aaron.m.curry84@gmail.com>
2026-05-22 20:47:05 +0100

    Fixing cors configuration

---
+++ src/main/java/travel/placesbeen/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package travel.placesbeen.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("!test")
+public class CorsConfig {
+    // The ':http://localhost:4200' acts as a default fallback value for local development
+    @Value("${app.cors.allowed-origins:http://localhost:4200}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Applies to all endpoints
+                        .allowedOrigins("http://localhost:4200") // Your Angular app
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
